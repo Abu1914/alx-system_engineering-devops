c7fad1e8716d5f742db2ba070b6c9c83ddd56e0c
Abu1914 <dmuktaru@gmail.com>
2024-03-27 14:28:30 +0000

    Install Flask

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,20 @@
-# Install Flask
-package { 'flask':
-    ensure   => '2.1.0',
-    provider => 'pip3',
+# Define a package resource to ensure Python 3 and pip3 are installed
+package { 'python3':
+  ensure => 'installed',
+}
+
+package { 'python3-pip':
+  ensure => 'installed',
+}
+
+# Define an exec resource to install Flask using pip3
+exec { 'install_flask':
+  command => '/usr/bin/pip3 install flask',
+  path    => ['/usr/bin', '/usr/local/bin'],
+  # Ensure the package is installed only if it's not already installed
+  unless  => '/usr/bin/pip3 show flask',
+  require => [
+    Package['python3'],
+    Package['python3-pip'],
+  ],
 }
